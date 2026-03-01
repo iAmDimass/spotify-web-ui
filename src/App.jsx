99d5dbd9c91481bb782422934bd97f3cb13c6e45
iAmDimass <orratto.com@gmail.com>
2026-03-01 15:39:04 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   const artworkUrl =
-    "https://instagram.fsrg5-2.fna.fbcdn.net/v/t39.30808-6/641024289_905891149039122_8055481058494607355_n.jpg?stp=c0.0.1536.1919a_dst-jpg_e35_s1536x1919_tt6&_nc_cat=104&ig_cache_key=Mzg0MTgxOTI4NDcyMDkxMjQ1MQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjE1MzZ4MTkyMC5zZHIuQzMifQ%3D%3D&_nc_ohc=HcHMMBhE_o8Q7kNvwHNsVzh&_nc_oc=Adk110KzaOL4LHk_ixQ33K0rSHse_gB3RcZs7XtNcGfY6xdtS3BK-N0tJ29exjp7Z-A&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fsrg5-2.fna&_nc_gid=eHwqSs_QLzXV-zC_PnhMmw&_nc_ss=8&oh=00_AfvUrxJpRfuoOY_KKJdOdlfYXManUxMX9-gXcKv876WbAA&oe=69A9BB4E";
+    "https://instagram.fsrg5-1.fna.fbcdn.net/v/t51.82787-15/634497238_18565576186035723_1226701140431012928_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=102&ig_cache_key=MzgzMTcwMDY4NDIyODMxMDc4OQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjEwODB4MTM1MC5zZHIuQzMifQ%3D%3D&_nc_ohc=mIjTb1950TAQ7kNvwE6XDg5&_nc_oc=Adl6si-apmV1EbBOvt1Y2kSrzAEPJ6e1yLu5iXdbKJyP4x7otf85nFKsQjhLpyTjDos&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fsrg5-1.fna&_nc_gid=FA3IH9BEGa5_3_ZpFRzmFg&_nc_ss=8&oh=00_AfskOgFJeVW11LlS3wvGkCPksDSSDBy43Pw6OMdz2h_NZA&oe=69A9B5EE";
 
   return (
     <div className="page">
